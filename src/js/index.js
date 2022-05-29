260bfd31a85f188eb32c2c3d4d2671d405582509
HSLeal <hsleal@gmail.com>
2022-05-29 00:25:19 -0300

    pronto

--- src/js/index.js
+++ src/js/index.js
@@ -13,40 +13,48 @@ const opcoes = document.querySelectorAll('.clicado');
 
 let click = 0;
 let imgTemp = [];
-let idTemp = [];
-let timer;
-opcoes.forEach((opcao) => {
+let idTemp = [-1,-1];
+let timer = 0;
+opcoes.forEach((opcao) => {    
     opcao.addEventListener('click', () => { 
-         
-        
-        if ((click === 2) && (imgTemp[0] !== imgTemp[1])){
-            idTemp[0].src = './src/img/hidden.jpg';
-            idTemp[1].src = './src/img/hidden.jpg'; 
+        const idClicado = opcao.attributes.id.value;                
+        if ((timer !== 0)) {
             clearTimeout(timer);
+            idTemp[0].src = './src/img/hidden.jpg';
+            idTemp[1].src = './src/img/hidden.jpg';
+            timer = 0;
             click = 0;
         }
-        const idClicado = opcao.attributes.id.value;
-        
         idTemp[click] = document.getElementById(`${idClicado}`);
         imgTemp[click] = `./src/img/${filhotes[idClicado]}.jpg`
-        idTemp[click].src = imgTemp[click];
+        idTemp[click].src = imgTemp[click];        
+        if (idTemp[0].id === idTemp[1].id) return click =1;    
         
-        if (click === 1) {            
-            if (imgTemp[0] !== imgTemp[1]) { 
-                timer = setTimeout(function(){
+        
+        if (click === 1) {
+            if ((imgTemp[0] !== imgTemp[1]) && 
+            (timer === 0)) { 
+        
+                timer = setTimeout(function(){  
+                click = 0;                  
                 idTemp[0].src = './src/img/hidden.jpg';
                 idTemp[1].src = './src/img/hidden.jpg';
+                timer = 0;                
+        
+                },2000);                                
+            } else if (imgTemp[0] === imgTemp[1]) {
                 click = 0;
-                console.log('chegamos aqui');                
-                },2000);                
-            } else {
-                click = -1;
+                clearTimeout(timer);
+                document.getElementById(idTemp[0].id).classList.add('revelado');
+                document.getElementById(idTemp[0].id).classList.remove('clicado');
+                document.getElementById(idTemp[1].id).classList.add('revelado');
+                document.getElementById(idTemp[1].id).classList.remove('clicado');
+                return;                
             }            
-        }         
-        click ++;
+        }           
         
+        if (click === 0) {click ++;}
         
-
     })
 })
 
